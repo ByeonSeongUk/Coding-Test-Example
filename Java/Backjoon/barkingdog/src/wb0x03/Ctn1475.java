e8134cac92695850cb074d653a8bac955f759b03
ByeonSeongUk <seonguk092@gmail.com>
2023-01-09 07:53:36 +0900

    ~문제 10807

--- Java/Backjoon/barkingdog/src/wb0x03/Ctn1475.java
+++ Java/Backjoon/barkingdog/src/wb0x03/Ctn1475.java
@@ -1,33 +1,39 @@
 package wb0x03;
-// 개수 세기
+// 방 번호
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.Stream;
 
 import static java.lang.Integer.parseInt;
 
-public class Ctn10807 {
+public class Ctn1475 {
 
     public static void main(String[] args) {
 
         FastReader fr = new FastReader();
 
-        int len = fr.nextInt();
-        int[] arr = new int[len];
+        int a = fr.nextInt();
+        int[] arr = Stream.of(String.valueOf(a).split("")).mapToInt(Integer::parseInt).toArray();
         int cnt = 0;
 
-        for(int i = 0; i < len; i++) {
-            arr[i] = fr.nextInt();
-        }
-        int v = fr.nextInt();
-
-        for(int i = 0; i < len; i++) {
-            if(arr[i] == v) cnt++;
+        int[] tmp = new int[9];
+        for(int i = 1; i < 10; i++) {
+            tmp[i-1] = i;
         }
 
-        System.out.println(cnt);
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < 8; j++) {
+                // if(arr[i] == j) cnt++;
+                for(int k = 0; k < arr.length; k++) {
+
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
 
     }
 
